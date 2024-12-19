4ad387671b5859735269ce889e04585678e06a9f
TVATDCI <tuanthong.vaidyanond@dci-student.org>
2024-12-19 14:00:05 +0100

    edited weather

--- weather.js
+++ weather.js
@@ -2,7 +2,16 @@ import chalk from "chalk";
 import fetch from "node-fetch";
 
 const city = process.argv[2];
-const unit = process.argv[3] === "imperial" ? "imperial" : "metric"; // Default to metric
+/**
+ *  Extracting city name from the command line arguments:
+ * - For example, if the user runs the program with the command "node weather.js London", the city variable will be set to London. Then, = process.argv[2] will be set to command line argument 2, which is London.
+ */
+const unit = process.argv[3] === "imperial" ? "imperial" : "metric";
+/**
+ * Determining unit of temperature from the command line arguments:
+ * - const unit = process.argv[3] is again extracting the third command line argument(argument 0 is node, argument 1 is the file name, argument 2 is the city name, and argument 3 is the unit of temperature).
+ * - Now === "imperial" ? is a ternary operator that checks if the unit in the command line argument is imperial. If it is, the unit variable is set to imperial; otherwise, it is set to metric (btw.by default).
+ */
 const port = process.env.PORT || 3003; // Default port is 3000
 
 if (!city) {
