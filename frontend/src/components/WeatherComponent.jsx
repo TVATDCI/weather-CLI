9f91f0645b6e4c80adaf4adbb2c6bcd0b45e0ca7
TVATDCI <tuanthong.vaidyanond@dci-student.org>
2025-02-14 01:15:34 +0100

    installed tailwind

---
+++ frontend/src/components/WeatherComponent.jsx
@@ -0,0 +1,13 @@
+// src/components/WeatherComponent.jsx
+import React from "react";
+
+const WeatherComponent = () => {
+  return (
+    <div>
+      <h1>Weather Information</h1>
+      {/* Weather details will be displayed here */}
+    </div>
+  );
+};
+
+export default WeatherComponent;
